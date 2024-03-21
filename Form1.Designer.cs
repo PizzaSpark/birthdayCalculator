986d3f9dd3be1169bc3b01b08f1e7deebe7a1497
Seanti <seanpizzaspark@gmail.com>
2024-03-22 01:40:19 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace birthday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.birthday = new System.Windows.Forms.DateTimePicker();
+            this.compareDate = new System.Windows.Forms.DateTimePicker();
+            this.compute = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birthday";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(107, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Compare";
+            // 
+            // birthday
+            // 
+            this.birthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthday.Location = new System.Drawing.Point(259, 98);
+            this.birthday.Name = "birthday";
+            this.birthday.Size = new System.Drawing.Size(382, 30);
+            this.birthday.TabIndex = 2;
+            // 
+            // compareDate
+            // 
+            this.compareDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compareDate.Location = new System.Drawing.Point(259, 163);
+            this.compareDate.Name = "compareDate";
+            this.compareDate.Size = new System.Drawing.Size(382, 30);
+            this.compareDate.TabIndex = 3;
+            // 
+            // compute
+            // 
+            this.compute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compute.Location = new System.Drawing.Point(527, 296);
+            this.compute.Name = "compute";
+            this.compute.Size = new System.Drawing.Size(114, 38);
+            this.compute.TabIndex = 4;
+            this.compute.Text = "Compute";
+            this.compute.UseVisualStyleBackColor = true;
+            this.compute.Click += new System.EventHandler(this.compute_Click);
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(259, 232);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(382, 25);
+            this.output.TabIndex = 5;
+            this.output.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.compute);
+            this.Controls.Add(this.compareDate);
+            this.Controls.Add(this.birthday);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker birthday;
+        private System.Windows.Forms.DateTimePicker compareDate;
+        private System.Windows.Forms.Button compute;
+        private System.Windows.Forms.Label output;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace birthday
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void compute_Click(object sender, EventArgs e)
+        {
+            DateTime dt1 = birthday.Value;
+            DateTime dt2 = compareDate.Value;
+
+            int months = GetTotalMonthsFrom(dt1, dt2);
+
+            output.Text = months.ToString();  
+
+        }
+
+        public static int GetTotalMonthsFrom(this DateTime dt1, DateTime dt2)
+        {
+            DateTime earlyDate = (dt1 > dt2) ? dt2.Date : dt1.Date;
+            DateTime lateDate = (dt1 > dt2) ? dt1.Date : dt2.Date;
+
+            // Start with 1 month's difference and keep incrementing
+            // until we overshoot the late date
+            int monthsDiff = 1;
+            while (earlyDate.AddMonths(monthsDiff) <= lateDate)
+            {
+                monthsDiff++;
+            }
+
+            return monthsDiff - 1;
+        }
+    }
+}
